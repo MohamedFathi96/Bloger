31e6b655f92dee28a90d1cbca0f0b3f3662b4c2b
MohamedFathi96 <mohamedfathi4345@gmail.com>
2023-03-19 07:52:25 +0200

    filtering is working

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import SignUp from "./pages/SignUp";
 import SideBar from "./components/SideBar";
 import NavBar from "./components/NavBar";
 import PostDetails from "./pages/PostDetails";
+import Alert from "@mui/material/Alert";
+import { AlertTitle, Fade } from "@mui/material";
 import WritePost from "./pages/WritePost";
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  useLocation,
+} from "react-router-dom";
 import { useAuthContext } from "./context/AuthContext";
 import Home from "./pages/Home";
 
 const Layout = () => {
+  // const message = useLocation().state;
   return (
     <>
       <SideBar />
@@ -56,7 +64,24 @@ function App() {
   return (
     <div className={`${mode}`}>
       <div className="fixed top-2 left-2 dark:text-white text-xl z-20 sm:before:content-['sm'] md:before:content-['md'] lg:before:content-['lg'] xl:b sm:before:content-['sm']efore:content-['xl'] 2xl:before:content-['2xl']"></div>
+      {/* {message && (
+        <Alert
+          variant="filled"
+          onClose={(e) => {
 
+          }}
+          sx={{
+            position: "fixed",
+            top: "30px",
+            left: "50%",
+            zIndex: 100,
+            transform: "translateX(-50%)",
+          }}
+          severity={`${message ? message.severity : ""}`}
+        >
+          {message.title}
+        </Alert>
+      )} */}
       {/* <MouseCanvas /> */}
       <RouterProvider router={router} />
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,14 @@
-import { onSnapshot, query, where } from "firebase/firestore";
-import React, { useState, useEffect } from "react";
+import { getDocs, limit, onSnapshot, query, where } from "firebase/firestore";
+import React, { useEffect } from "react";
 import Post from "../components/Post";
 import { useDatabaseContext } from "../context/DatabaseContext";
-import Alert from "@mui/material/Alert";
-import { AlertTitle, Fade } from "@mui/material";
-import { useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const Home = () => {
-  const { postsRef, posts, setposts, searchValue } = useDatabaseContext();
-  const message = useLocation().state;
+  const { postsRef, posts, setposts } = useDatabaseContext();
+  const { category } = useParams();
   useEffect(() => {
-    const postQuery = query(postsRef);
+    const postQuery = query(postsRef, limit(10));
     onSnapshot(postQuery, (snapshot) => {
       const articles = snapshot.docs.map((doc) => ({
         id: doc.id,
@@ -18,27 +16,23 @@ const Home = () => {
       }));
       setposts(articles);
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  useEffect(() => {
+    const postQuery = category
+      ? query(postsRef, where("category", "==", category), limit(10))
+      : query(postsRef, limit(10));
+    getDocs(postQuery).then((snapshot) => {
+      const articles = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setposts(articles);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [category]);
   return (
     <div>
-      {/* {message && (
-        <Alert
-          variant="filled"
-          onClose={(e) => {
-
-          }}
-          sx={{
-            position: "fixed",
-            top: "30px",
-            left: "50%",
-            zIndex: 100,
-            transform: "translateX(-50%)",
-          }}
-          severity={`${message ? message.severity : ""}`}
-        >
-          {message.title}
-        </Alert>
-      )} */}
       <div className="ml-56 p-12 flex items-center min-h-screen flex-col gap-4 dark:text-white dark:bg-main-dark-bg">
         {posts.map((postData) => (
           <Post key={postData.id} data={postData} />
